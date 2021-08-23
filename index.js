b99a802f4316a234c8d01dd4fe8c3fe94827af51
猴子 <70706886+a3510377@users.noreply.github.com>
2021-08-23 11:36:41 +0000

    東搞搞西搞搞

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
 const express = require("express")
 const fetch = require("node-fetch");
 const cors = require("cors");
-const functions = require('./functions.js');
-const { AuthorizationXBL } = require('./RPMLauncher/MSAccountHandler');
+const functions = require("./functions")
 
-const Token = {
-  client_id: "8HpxK2jINouRXTrVq6gf",
-  client_secret: process.env.client_secret,
-  redirect_uri: "https://rear-end.a102009102009.repl.co/crowdin/oauth/auth/web",
-  web: "https://www.rpmtw.ga/Translator"
-}
 
 express()
-  .use(cors())
-  .set("view engine", "ejs")
-  .get("/", (req, res) => {
-    res.send("此為 RPMTW 後端 API 非工作人員請勿訪問")
-  })
-  .get("/crowdin/oauth/auth/web", (req, res) => {
-    /* 回傳token */
-    let params = req.query
-    functions.getToken(params.code, res)
-  })
-  .get("/curseForge/api/", (req, res) => {
-    /* CurseForge API 代理 */
-    fetch(`https://addons-ecs.forgesvc.net/api/v2/${req.query.url}`)
-      .then(d => d.json())
-      .then(json => res.json(json))
-      .catch((error) => res.json({ error: error }))
-  })
-  .get("/rpmlauncher/api/microsof-auth", async (req, res) => {
-    await AuthorizationXBL(req.query['accessToken']).then(json => {
-      res.json(json);
+    .use(cors())
+    .set("view engine", "ejs")
+    .get("/", (req, res) => {
+        res.send("此為 RPMTW 後端 API 非工作人員請勿訪問")
     })
-  })
+    .get("/crowdin/oauth/auth/web", (req, res) => {
+        /* 回傳token */
+        let params = req.query
+        functions.getToken(params.code, res)
+    })
+    .get("/curseForge/api/", (req, res) => {
+        /* CurseForge API 代理 */
+        fetch(`https://addons-ecs.forgesvc.net/api/v2/${req.query.url}`)
+            .then(d => d.json())
+            .then(json => res.json(json))
+            .catch((error) => res.json({ error: error }))
+    })
+    .get("/rpmlauncher/api/microsof-auth", (req, res) => {
+        functions.AuthorizationXBL(req.query.accessToken)
+    })
+    .get("/rpmtranslator/api/deepl-translator", async (req, res) => {
 
-  .get("/rpmtranslator/api/deepl-translator", async (req, res) => {
-
-  })
-  .listen(process.env.PORT || 5000, () => {
-    console.log("is ready")
-  })
+    })
+    .listen(process.env.PORT || 5000, () => {
+        console.log("is ready")
+    })
