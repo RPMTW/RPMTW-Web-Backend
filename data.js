dfe7f1bc8cc9dd3ac667e9a3efe81e4d50631586
a3510377 <a102009102009@gmail.com>
2021-08-27 23:16:14 +0800

    test

--- data.js
+++ data.js
@@ -4,31 +4,36 @@
         X: https://test.com/
 */
 
-function sets() {
-    this.web = {
+let sets_main = {
+    web: {
         api: "https://rear-end.a102009102009.repl.co",
         main: "https://www.rpmtw.ga",
         translator: "https://translator.rpmtw.ga/index.html"
-    }
-    this.discord = {
+    },
+    discord: {
         botId: "880821114014666753",
         client_secret: process.env.discord_client_secret,
-    }
-    this.crowdin = {
+    },
+    crowdin: {
         /* crowdin */
         client_id: "8HpxK2jINouRXTrVq6gf",
         client_secret: process.env.crowdin_client_secret,
         redirect_uri: "https://rear-end.a102009102009.repl.co/crowdin/oauth/auth/web",
-    }
-    this.API = {
+    },
+    API: {
         /* 其它API */
     }
-    this.oauth2 = {
-        discord: `https://discord.com/api/oauth2/authorize?response_type=code&client_id=${this.discord.botId}&scope=identify%20guilds.join&redirect_uri=${this.web.api + "/discord/oauth/auth"}&prompt=consent`
+}
+let sets_rear = {
+    discord: {
+        Oauth2: `https://discord.com/api/oauth2/authorize?response_type=code&client_id=${sets.discord.botId}&scope=identify%20guilds.join&redirect_uri=${sets.web.api + "/discord/oauth/auth"}&prompt=consent`
     }
 }
-sets = sets()
-console.log(sets.oauth2.discord)
+let sets = {
+    ...sets_main,
+    ...sets_rear
+}
+console.log(sets.discord.Oauth2)
 module.exports = {
     sets
 }
