504696fee64199a8c3b3cee307f5c2e207192324
a3510377 <a102009102009@gmail.com>
2021-08-27 23:11:50 +0800

    test

--- data.js
+++ data.js
@@ -13,7 +13,6 @@ function sets() {
     this.discord = {
         botId: "880821114014666753",
         client_secret: process.env.discord_client_secret,
-        Oauth2: `https://discord.com/api/oauth2/authorize?response_type=code&client_id=${this.discord.botId}&scope=identify%20guilds.join&redirect_uri=${this.web.api + "/discord/oauth/auth"}&prompt=consent`
     }
     this.crowdin = {
         /* crowdin */
@@ -24,9 +23,12 @@ function sets() {
     this.API = {
         /* 其它API */
     }
+    this.oauth2 = {
+        discord: `https://discord.com/api/oauth2/authorize?response_type=code&client_id=${this.discord.botId}&scope=identify%20guilds.join&redirect_uri=${this.web.api + "/discord/oauth/auth"}&prompt=consent`
+    }
 }
 sets = sets()
-console.log(sets.discord.Oauth2)
+console.log(sets.oauth2.discord)
 module.exports = {
     sets
 }
