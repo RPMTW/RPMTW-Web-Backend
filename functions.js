91e6aa9a7819f622dd86b19dfa80ecf14405049f
菘菘 <rrt467778@gmail.com>
2021-08-27 11:28:04 +0000

    Update

--- functions.js
+++ functions.js
@@ -1,150 +1,32 @@
 const fetch = require("node-fetch");
 const Token = {
-    client_id: "8HpxK2jINouRXTrVq6gf",
-    client_secret: process.env.client_secret,
-    redirect_uri: "https://rear-end.a102009102009.repl.co/crowdin/oauth/auth/web",
-    web: "https://www.rpmtw.ga/Translator",
+  client_id: "8HpxK2jINouRXTrVq6gf",
+  client_secret: process.env.client_secret,
+  redirect_uri: "https://rear-end.a102009102009.repl.co/crowdin/oauth/auth/web",
+  web: "https://translator.rpmtw.ga",
 };
 
-module.exports = {
-    getToken: (code, res) =>
-        /* 抓取Token重導向至 `${Token.web}` */
-        fetch(`https://accounts.crowdin.com/oauth/token`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                grant_type: "authorization_code",
-                client_id: Token.client_id,
-                client_secret: Token.client_secret,
-                redirect_uri: Token.redirect_uri,
-                code: code,
-            }),
-        })
-            .then((d) => d.json())
-            .then((json) => {
-                json =
-                    (json.error && {
-                        type: "error",
-                        data: json.error,
-                    }) ||
-                    (json.access_token && {
-                        type: "data",
-                        data: json.access_token,
-                    });
-                res.redirect(301, `${Token.web}?${json.type}=${json.data}`);
-            })
-            .catch((error) => res.send(error)),
-    AuthorizationXBL: (accessToken) =>
-        fetch("https://user.auth.xboxlive.com/user/authenticate", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify({
-                Properties: {
-                    AuthMethod: "RPS",
-                    SiteName: "user.auth.xboxlive.com",
-                    RpsTicket: `d=${accessToken}`,
-                },
-                RelyingParty: "http://auth.xboxlive.com",
-                TokenType: "JWT",
-            }),
-            redirect: "follow",
-        })
-            .then((res) => res.json())
-            .then((json) => this.Authorizations.AuthorizationXSTS(json.Token)),
-    Authorizations: {
-        AuthorizationXSTS: (xblToken) =>
-            fetch("https://xsts.auth.xboxlive.com/xsts/authorize", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify({
-                    Properties: {
-                        SandboxId: "RETAIL",
-                        UserTokens: [xblToken],
-                    },
-                    RelyingParty: "rp://api.minecraftservices.com/",
-                    TokenType: "JWT",
-                }),
-                redirect: "follow",
-            })
-                .then((res) => res.json())
-                .then((json) =>
-                    this.Authorizations.AuthorizationMinecraft(
-                        json.Token,
-                        json.DisplayClaims.xui[0].uhs
-                    )
-                ),
-        AuthorizationMinecraft: (xstsToken, UserHash) =>
-            fetch(
-                "https://api.minecraftservices.com/authentication/login_with_xbox",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                    },
-                    body: JSON.stringify({
-                        identityToken: `XBL3.0 x=${UserHash};${xstsToken}`,
-                    }),
-                    redirect: "follow",
-                }
-            )
-                .then((res) => res.json())
-                .then((json) =>
-                    this.Authorizations.CheckingGameOwnership(json.access_token)
-                ),
-        CheckingGameOwnership: (MCAccessToken) =>
-            fetch("https://api.minecraftservices.com/entitlements/mcstore", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify({
-                    Properties: {
-                        SandboxId: "RETAIL",
-                        UserTokens: [xblToken],
-                    },
-                    RelyingParty: "rp://api.minecraftservices.com/",
-                    TokenType: "JWT",
-                }),
-                redirect: "follow",
-            })
-                .then((res) => res.json())
-                .then((json) => {
-                    try {
-                        if (JSON.parse(json).items.length > 0) {
-                            let profileJson = this.Authorizations.GetProfile(MCAccessToken);
-                            let profile = { name: profileJson.name, id: profileJson.id };
-                            return [{
-                                accessToken: MCAccessToken,
-                                selectedProfile: profile,
-                                availableProfile: [profile],
-                            }];
-                        }
-                        return [];
-                    } catch (e) {
-                        return [];
-                    }
-                }),
-        GetProfile: (MCAccessToken) => {
-            fetch("https://api.minecraftservices.com/entitlements/mcstore", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${MCAccessToken}`,
-                },
-                redirect: "follow",
-            })
-                .then((response) => response.json())
-                .then((result) => result)
-        },
+let getToken = (code, res) => {
+  /* 抓取Token並重導向至 `${Token.web}/callback` */
+  fetch(`https://accounts.crowdin.com/oauth/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      client_id: Token.client_id,
+      client_secret: Token.client_secret,
+      redirect_uri: Token.redirect_uri,
+      code: code,
+    }),
+  })
+    .then((d) => d.json())
+    .then((json) => {
+      console.log(json)
+      res.redirect(301, `${Token.web}/callback.html?data=${JSON.stringify(json)}`);
+    })
+}
+module.exports = {
+  getToken
 };
--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 const express = require("express")
 const fetch = require("node-fetch");
 const cors = require("cors");
-const functions = require("./functions")
+const { getToken } = require("./functions")
+const { AuthorizationXBL } = require("./MSAccountHandler")
 
 express()
-    .use(cors())
-    .set("view engine", "ejs")
-    .get("/", (req, res) => {
-        res.send("此為 RPMTW 後端 API 非工作人員請勿訪問")
+  .use(cors())
+  .set("view engine", "ejs")
+  .get("/", (req, res) => {
+    res.send("此為 RPMTW 後端 API 非工作人員請勿訪問 www")
+  })
+  .get("/crowdin/oauth/auth/web", (req, res) => {
+    /* 回傳token */
+    let params = req.query
+    getToken(params.code, res)
+  })
+  .get("/curseForge/api/", (req, res) => {
+    /* CurseForge API 代理 */
+    fetch(`https://addons-ecs.forgesvc.net/api/v2/${req.query.url}`)
+      .then(d => d.json())
+      .then(json => res.json(json))
+      .catch((error) => res.json({ error: error }))
+  })
+  .get("/crowdin/api/", (req, res) => {
+    /* Crowdin API 代理 */
+    fetch(`https://api.crowdin.com/api/v2/${req.query.url}`, {
+      headers: req.headers.authorization == null ? {} : {"Authorization" : req.headers.authorization}
     })
-    .get("/crowdin/oauth/auth/web", (req, res) => {
-        /* 回傳token */
-        let params = req.query
-        functions.getToken(params.code, res)
+      .then(d => d.json())
+      .then(json => res.json(json))
+      .catch((error) => res.json({ error: error }))
+  })
+  .get("/rpmlauncher/api/microsof-auth", async (req, res) => {
+    await AuthorizationXBL(req.query['accessToken']).then(json => {
+      res.json(json);
     })
-    .get("/curseForge/api/", (req, res) => {
-        /* CurseForge API 代理 */
-        fetch(`https://addons-ecs.forgesvc.net/api/v2/${req.query.url}`)
-            .then(d => d.json())
-            .then(json => res.json(json))
-            .catch((error) => res.json({ error: error }))
-    })
-    .get("/rpmlauncher/api/microsof-auth", (req, res) => {
-        functions.AuthorizationXBL(req.query.accessToken)
-            .done(data => res.json(data))
-    })
-    .get("/rpmtranslator/api/deepl-translator", async (req, res) => {
+  })
 
-    })
-    .listen(process.env.PORT || 5000, () => {
-        console.log("is ready")
-    })
+  .get("/rpmtranslator/api/deepl-translator", async (req, res) => {
+
+  })
+  .listen(process.env.PORT || 5000, () => {
+    console.log("is ready")
+  })
