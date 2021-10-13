f65cd4501ff759aba7572f5798e3609b1e076d70
猴子 <70706886+a3510377@users.noreply.github.com>
2021-10-13 11:44:11 +0000

    yes!!!!

--- router/API/Discord.js
+++ router/API/Discord.js
@@ -17,17 +17,14 @@ router
         /* 回傳 discord token */
         if (!req.query.code) return res.status(400).json(BadRequestError())
         await getDiscordToken(req.query.code)
-            .then(data => {
-                console.log(data)
-                res.json(data.json())
-            })
-            .catch(error => {
-                console.error(error)
+            .then(data => data.json())
+            .then(json => res.json(json))
+            .catch(error => 
                 res.status((error.response && error.response.status) || 500).json({
                     message: error.message,
                     name: error.name,
                 })
-            })
+            )
     })
 
 module.exports = router;
