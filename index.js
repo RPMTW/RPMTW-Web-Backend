7c53175be67540d10de6a46616fc0bc0865f00ad
a3510377 <a102009102009@gmail.com>
2021-10-16 13:54:48 +0800

    add ping

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 require("dotenv").config()
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
+const ping = require("express-ping");
 
+const router = require("./router");
 const {
     Server
 } = require("http");
 const {
-    createServer: createViteServer
-} = require("vite")
-
+    discord
+} = require("./data")
 const client = require("./client")
-const router = require("./router");
 
 const app = express();
+
 const server = Server(app);
 
-async function createServer() {
-    const vite = await createViteServer({
-        server: {
-            middlewareMode: "ssr"
-        }
-    });
+app
+    .use(cors())
+    .use(ping.ping())
+    .set("view engine", "html")
+    .get("/", (req, res) => res.send("此為 RPMTW 後端 API 非工作人員請勿訪問 www"))
+    .use(router)
 
-    app
-        /* --------------- */
-        .use(require("cors")())
-        .set("views", path.join(__dirname, "views"))
-        .set("view engine", "html")
-        .use(express.static(path.join(__dirname, "public")))
-        /* --------------- */
-        .get("/", (req, res) => res.send("此為 RPMTW 後端 API 非工作人員請勿訪問 www"))
-        .use(router)
-        .use(vite.middlewares)
+server.listen(process.env.PORT || 5000, () => {
+    console.log("is ready")
+    client.login(process.env.discord_bot_token);
+})
 
-    server.listen(process.env.PORT || 5000, () => {
-        console.log("is ready")
-        client.login(process.env.discord_bot_token);
-    });
-};
-
-createServer();
+console.log(discord.Oauth2)
